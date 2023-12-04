5bab9d2d1b856fb8b17491968de070b69246f5d6
Dmytro Gaiduk <8593269+DmytroGaiduk@users.noreply.github.com>
2023-12-04 16:16:56 +0100

    remove unused comment

--- src/context/CounterContext.tsx
+++ src/context/CounterContext.tsx
@@ -93,7 +93,6 @@ function CounterContextProvider({children}: any) {
         });
     }
 
-
     //error
     const errorHandler = (error: string | null) => {
         dispatch({
@@ -119,58 +118,3 @@ function CounterContextProvider({children}: any) {
 }
 
 export default CounterContextProvider;
-
-
-// const counterReducer = (state: any, action: any): any => {
-//
-//     const {type} = action;
-//
-//     switch (type) {
-//         case 'increment': {
-//             if (state.value < state.maximumValue) {
-//                 return {...state, value: state.value + 1, errorMessage: null};
-//             } else {
-//                 return {...state, errorMessage: 'Cannot increment bigger than the maximum value.'};
-//             }
-//         }
-//         case 'decrement': {
-//             if (state.value > state.minimumValue) {
-//                 return {...state, value: state.value - 1, errorMessage: null};
-//             } else {
-//                 return {...state, errorMessage: 'Cannot decrement below the minimum value.'};
-//             }
-//         }
-//         case 'reset': {
-//             return {...state, value: counterData.initialValue, errorMessage: null};
-//         }
-//         case 'update-maximum': {
-//             if (action.payload > state.minimumValue) {
-//                 return {...state, maximumValue: action.payload, errorMessage: null};
-//             } else {
-//
-//                 return {...state, errorMessage: 'Maximum value must be greater than the minimum value.'};
-//             }
-//         }
-//         case 'update-minimum':{
-//             if (action.payload < state.maximumValue) {
-//                 return {...state, minimumValue: action.payload, errorMessage: null};
-//             } else {
-//                 return {...state, errorMessage: 'Minimum value must be smaller than the maximum value.'};
-//             }
-//         }
-//         case 'update-start': {
-//             if (action.payload >= state.minimumValue && action.payload <= state.maximumValue) {
-//                 return {...state, value: action.payload, errorMessage: null};
-//             } else {
-//                 return {
-//                     ...state,
-//                     errorMessage: 'Start value must be greater than minimum value and lesser than the maximum value'
-//                 }
-//             }
-//         }
-//         case 'update-error-message':
-//             return {...state, errorMessage: action.payload}
-//         default:
-//             return state;
-//     }
-// };
